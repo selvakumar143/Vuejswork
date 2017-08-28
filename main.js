70068dbc4120c497ae3d49768a4c6d979f3a8c14
selvakumar <sp.selvakumar2012@gmail.com>
2017-08-28 16:47:51 +0530

    8th

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+Vue.component('task-list',{
+	template:`
+		<div>
+		<task v-for="task in tasks">{{task.description}}</task>`,
+		data(){
+			return {
+		tasks:[
+			{description: 'store',completed:true},						
+			{description: 'shopping',completed:true},						
+			{description: 'purchase',completed:false},						
+			{description: 'pay',completed:false},						
+			{description: 'visting',completed:true}
+			]
+		};
+		}
+});
+
 Vue.component('task',{
 	template:'<li><slot></slot></li>'
 });
