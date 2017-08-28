e9e808e2bb3f5c1b7245e461266ece901aa3cd77
selvakumar <sp.selvakumar2012@gmail.com>
2017-08-28 22:07:38 +0530

    close function

---
+++ 9th.js
@@ -0,0 +1,21 @@
+Vue.component('message',{
+	props:['title','body'],
+	data(){
+		return{
+			isVisible:true
+		};
+	},
+	template:`
+		<article class="message" v-show="isVisible">
+		<div class="message-header">
+			{{title}}			
+			<button type="button" @click=" isVisible = false">X</button>
+		</div>
+		
+		<div class="message-body">
+			{{body}}
+		</div>
+		</article>`
+		});new Vue({
+	el:'#root'
+});
